c4040d9955104f7a57b685f815a0c9a77726330a
Daniel Silva Ferreira <danielsilva@usp.br>
2023-10-19 10:18:44 -0300

    Add files via upload

---
+++ copyrightHolder.php
@@ -0,0 +1,36 @@
+<?php
+
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+class copyrightHolder extends GenericPlugin {
+    public function register($category, $path, $mainContextId = NULL) {
+        $success = parent::register($category, $path);
+            if ($success && $this->getEnabled()) {
+               
+            }
+        return $success;
+    }
+
+  /**
+   * Provide a name for this plugin
+   *
+   * The name will appear in the plugins list where editors can
+   * enable and disable plugins.
+   */
+	public function getDisplayName() {
+		return __('plugins.generic.copyrightholder.displayName');
+	}
+
+	/**
+   * Provide a description for this plugin
+   *
+   * The description will appear in the plugins list where editors can
+   * enable and disable plugins.
+   */
+	public function getDescription() {
+		return __('plugins.generic.copyrightholder.description');
+	}
+	
+	
+
+}
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('copyrightHolder.php');
+return new copyrightHolder();
